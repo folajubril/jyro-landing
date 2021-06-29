1b00500ea1c6dbf1ae0faa2dec67009ceb35a5d3
adetiwa <adetiwa1@gmail.com>
2021-06-29 13:08:55 +0100

    update

--- header.php
+++ header.php
@@ -2,8 +2,19 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Jyro | App</title>
-	
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description"
+        content="Jyro - Get Daily Errands Done Easier Than Before. | Order items from accredited stores and get them delivered on time.| Get dedicated servicemen to run your errands while you focus on your daily works." />
+    <meta name="keywords"
+        content="Jyro  errand, pickup delivery service app " />
+    <meta name="author" content="Stackonly" />
+
+    <title>Jyro - Errand App</title>
+
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" href="img/favicon.png" type="image/gif" sizes="16x16">
 	<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
