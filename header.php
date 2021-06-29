f4da14429bcf5025922ef7c95443bc43d082e5db
adetiwa <adetiwa1@gmail.com>
2021-06-29 13:14:08 +0100

    update

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
     <title>Jyro - Errand App</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="img/favicon.png" type="image/gif" sizes="16x16">
+	<link rel="icon" href="img/favicon.ico" type="image/gif" sizes="16x16">
 	<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
 	<link rel="stylesheet" type="text/css" href="css/vendor.bundle.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
